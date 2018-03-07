40cd224a5077bbda018a70fd50773c8254e871af
dom-o <m.domonic+github@gmail.com>
2018-03-07 09:33:01 -0500

    Redirect to app from root URL.

--- soon/urls.py
+++ soon/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import include, path
+from django.views.generic.base import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('todo/', include('todo.urls')),
+    path('', RedirectView.as_view(url='todo/'))
 ]
