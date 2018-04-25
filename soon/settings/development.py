4983ff73b2c72d3ef9163bdee016962e941278f5
dom-o <m.domonic+github@gmail.com>
2018-04-24 20:04:08 -0400

    Settings

--- soon/settings/development.py
+++ soon/settings/development.py
@@ -4,3 +4,13 @@ import os
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.environ['DB_NAME'],
+        'USER': os.environ['DB_USER'],
+        'PASSWORD': os.environ['DB_PASSWORD'],
+        'HOST': os.environ['DB_HOST'],
+        'PORT': os.environ['DB_PORT'],
+    }
+}
--- soon/settings/production.py
+++ soon/settings/production.py
@@ -1,5 +1,7 @@
 from soon.settings.common import *
 import os
+import dj_database_url
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
@@ -12,3 +14,5 @@ SESSION_COOKIE_SECURE = True
 CSRF_COOKIE_SECURE = True
 
 X_FRAME_OPTIONS = 'DENY'
+
+DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
